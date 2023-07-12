524899609ab3739c7940212d0ea32b11deb18f3e
vliso <vlisovski26@gmail.com>
2023-07-12 14:44:34 +0300

    initialize project

---
+++ src/main/java/com/lisovski/mrmuscule/MrmusculeApplication.java
@@ -0,0 +1,13 @@
+package com.lisovski.mrmuscule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrmusculeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MrmusculeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lisovski/mrmuscule/MrmusculeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisovski.mrmuscule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrmusculeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

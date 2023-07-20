4fa504ecff448afed670fa2699cc981a604002dd
Vladislav <vlisovski26@gmail.com>
2023-07-20 22:55:29 +0300

    remove TODO

--- src/main/java/com/lisovski/mrmuscule/controllers/OrderController.java
+++ src/main/java/com/lisovski/mrmuscule/controllers/OrderController.java
@@ -39,41 +39,4 @@ public class OrderController {
         //операция добавления продуктов, входящих в заказ, в таблицу orders_products
         ordersProductsList.forEach((ordersProducts -> ordersProductsService.addOrderProductPair(ordersProducts)));
     }
-
-    //repository(data bases)->service->controller(url)
-
-    //TODO сделать добавление заказа(Order)
-    //public int addOrder(Order order)
-    //разобраться и написать метод, который получит с фронта объект Order с продуктами внутри и добавит его в БД
-    //тестировать в ARC
-    //не забыть создать новую ветку!!!!!!!!!!!
-    //Структура объекта с фронта:(Примерчик)
-    //{
-    //    "id": 1,
-    //    "date": "2023-07-17",
-    //    "cost": 5300,
-    //    "description": "Доставить к двери",
-    //    "user_id": 1,
-    //    "status": "delivered",
-    //    "ordersList": [
-    //      {
-    //        "id": 2,
-    //        "name": "Штанга",
-    //        "description": "Современная стальная штанага",
-    //        "price": 3500,
-    //        "type": "inventory",
-    //        "photoPath": "https://photolol.com"
-    //      },
-    //      {
-    //        "id": 3,
-    //        "name": "Протеин",
-    //        "description": "FoodWay двойной шоколад",
-    //        "price": 1800,
-    //        "type": "food",
-    //        "photoPath": "https://photolol.com"
-    //      }
-    //    ]
-    // }
-    //http://localhost:8080/orders/addOrder - URL , который должен работать по итогу на добавление заказа
-    //раз добавление происходит в две таблицы за 1 запрос, то подумай насчет применения аннотации @Transactional
 }
